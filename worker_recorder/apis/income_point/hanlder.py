23c9bc809d9f95ab48a3818902c5538081637808
zizle <462894999@qq.com>
2021-01-15 13:26:32 +0800

    收入指标统计

--- worker_recorder/apis/income_point/hanlder.py
+++ worker_recorder/apis/income_point/hanlder.py
@@ -7,55 +7,110 @@ import datetime
 import pandas as pd
 from db import DBWorker
 
-# 获取所有客户
-def get_customers():
+
+# 获取指定用户的所有客户和权益记录信息(管理员获取所有)
+def get_customers_and_revenues(start_timestamp: int, end_timestamp: int, author_id: int):
     with DBWorker() as (_, cursor):
         # 查询所有客户
         cursor.execute(
             "SELECT csttb.id AS customer_id,csttb.create_time AS customer_create,csttb.author_id,"
             "csttb.customer_name,usetb.username "
             "FROM work_customer AS csttb "
-            "INNER JOIN user_user AS usetb ON csttb.author_id=usetb.id;"
+            "INNER JOIN user_user AS usetb ON csttb.author_id=usetb.id "
+            "WHERE IF(0=%s,TRUE,csttb.author_id=%s);",
+            (author_id, author_id)
         )
         customers = cursor.fetchall()
-    return customers
-
-
-# 获取客户的最新权益记录
-def get_customer_revenue():
-    with DBWorker() as (_, cursor):
+        # 连表查询所有权益记录
         cursor.execute(
-            "SELECT t.create_time AS revenue_create,t.customer_id,t.remain,t.interest,t.crights "
-            "FROM ("
-            "SELECT customer_id,max(create_time) as create_time FROM work_customer_index GROUP BY customer_id"
-            ") AS a "
-            "INNER JOIN work_customer_index AS t "
-            "ON t.customer_id=a.customer_id AND t.create_time=a.create_time;"
+            "SELECT * "
+            "FROM work_customer_index AS revetb "
+            "INNER JOIN work_customer AS csttb ON revetb.customer_id=csttb.id "
+            "WHERE revetb.create_time>=%s AND revetb.create_time<%s AND IF(0=%s,TRUE,csttb.author_id=%s);",
+            (start_timestamp, end_timestamp, author_id, author_id)
         )
+        revenues = cursor.fetchall()
+    return customers, revenues
+
+
+# 获取指定日期中的客户权益记录信息(管理员获取所有)
+def get_customer_revenue(start_timestamp: int, end_timestamp: int, author_id: int):
+    with DBWorker() as (_, cursor):
+        # 获取最新的权益记录条目
+        # cursor.execute(
+        #     "SELECT t.create_time AS revenue_create,t.customer_id,t.remain,t.interest,t.crights "
+        #     "FROM ("
+        #     "SELECT customer_id,max(create_time) as create_time FROM work_customer_index GROUP BY customer_id"
+        #     ") AS a "
+        #     "INNER JOIN work_customer_index AS t "
+        #     "ON t.customer_id=a.customer_id AND t.create_time=a.create_time;"
+        # )
+        # 获取所有指定的权益记录信息
         customer_revenue = cursor.fetchall()
     return customer_revenue
 
-
+# 处理获取客户及其权益,收入(留存 + 利息)
 def handle_customer_amount_revenue(customer_df, revenue_df):
     if customer_df.empty:
         return []
-    # 转日期格式
-    customer_df['customer_create'] = customer_df['customer_create'].apply(lambda x: datetime.datetime.fromtimestamp(x).strftime('%Y-%m-%d'))
-    revenue_df['revenue_create'] = revenue_df['revenue_create'].apply(lambda x: datetime.datetime.fromtimestamp(x).strftime('%Y-%m-%d'))
-    # 以customer_id为键横向合并数据框
-    result_df = pd.merge(customer_df, revenue_df, on=['customer_id'], how='left')
-    # 统计数量
+    if revenue_df.empty:
+        row_count = customer_df.shape[0]
+        # 直接添加每个客户的留存，利息，权益都为空
+        customer_df['sum_remain'] = [0 for _ in range(row_count)]
+        customer_df['sum_interest'] = [0 for _ in range(row_count)]
+        customer_df['crights'] = [0 for _ in range(row_count)]
+        customer_df['create_time'] = ['' for _ in range(row_count)]
+        result_df = customer_df
+    else:
+        # 处理留存和利息权益的问题：留存和利息以客户为主累计加和，权益取create_time最大的一条
+        sum_revenue_df = revenue_df.groupby(by=['customer_id'], as_index=False)[['remain', 'interest']].sum()
+        sum_revenue_df = sum_revenue_df.reset_index()
+        sum_revenue_df = sum_revenue_df.rename(columns={'remain': 'sum_remain', 'interest': 'sum_interest'})
+        # 这是留存和利息的加和数据
+        # 排序后分组取第一条
+        rights_df = revenue_df.sort_values('create_time', ascending=False).groupby(by=['customer_id'], as_index=False).first()
+        rights_df = rights_df[['create_time', 'customer_id', 'author_id', 'crights']]  # 取需要的列
+        # 合并留存，利息和权益数据框
+        result_revenue_df = pd.merge(sum_revenue_df, rights_df, on=['customer_id'], how='left')
+        # 转create_time格式
+        result_revenue_df['create_time'] = result_revenue_df['create_time'].apply(
+            lambda x: datetime.datetime.fromtimestamp(x).strftime('%Y-%m-%d'))
+        # 再与客户数据合并
+        result_df = pd.merge(customer_df, result_revenue_df, on=['customer_id', 'author_id'], how='left')
+        # 填0
+        result_df.fillna(0, inplace=True)
+
+    # 最后转客户的创建日期格式
+    result_df['customer_create'] = result_df['customer_create'].apply(
+        lambda x: datetime.datetime.fromtimestamp(x).strftime('%Y-%m-%d'))
     # 1 计算各人员的客户数量
     amount_count_df = result_df.groupby(['author_id', 'username'], as_index=False)['author_id'].agg(
         {'total_count': 'count'})
-    # print(amount_count_df)
     # 2 计算各人员的客户所有权益和
     sum_revenue_df = result_df.groupby(by=['author_id', 'username'], as_index=False)[
-        ['remain', 'interest', 'crights']].sum()
-    # print(sum_revenue_df)
+        ['sum_remain', 'sum_interest', 'crights']].sum()
+
     # 合并数量和权益和的数据框
-    result_df = pd.merge(amount_count_df, sum_revenue_df, on=['author_id', 'username'], how='inner')
-    return result_df.to_dict(orient='records')
+    result_data_df = pd.merge(amount_count_df, sum_revenue_df, on=['author_id', 'username'], how='inner')
+    return result_data_df.to_dict(orient='records')
+
+    # # 转日期格式
+    # customer_df['customer_create'] = customer_df['customer_create'].apply(lambda x: datetime.datetime.fromtimestamp(x).strftime('%Y-%m-%d'))
+    # revenue_df['revenue_create'] = revenue_df['revenue_create'].apply(lambda x: datetime.datetime.fromtimestamp(x).strftime('%Y-%m-%d'))
+    # # 以customer_id为键横向合并数据框
+    # result_df = pd.merge(customer_df, revenue_df, on=['customer_id'], how='left')
+    # # 统计数量
+    # # 1 计算各人员的客户数量
+    # amount_count_df = result_df.groupby(['author_id', 'username'], as_index=False)['author_id'].agg(
+    #     {'total_count': 'count'})
+    # # print(amount_count_df)
+    # # 2 计算各人员的客户所有权益和
+    # sum_revenue_df = result_df.groupby(by=['author_id', 'username'], as_index=False)[
+    #     ['remain', 'interest', 'crights']].sum()
+    # # print(sum_revenue_df)
+    # # 合并数量和权益和的数据框
+    # result_df = pd.merge(amount_count_df, sum_revenue_df, on=['author_id', 'username'], how='inner')
+    # return result_df.to_dict(orient='records')
 
 
 
--- worker_recorder/apis/income_point/statistics.py
+++ worker_recorder/apis/income_point/statistics.py
@@ -6,18 +6,33 @@
 # 收入指标统计(每个用户的客户数量和最新权益记录)
 
 import pandas as pd
-from fastapi import APIRouter
-from .hanlder import get_customers, get_customer_revenue, handle_customer_amount_revenue
+from fastapi import APIRouter, Query, HTTPException
+from utils.time_handler import get_month_range, get_year_range
+from .hanlder import get_customers_and_revenues, handle_customer_amount_revenue
 
 statistics_api = APIRouter()
 
 
-@statistics_api.get('/')
-async def statistics_customer_revenue():  # 统计每个用户的客户数量和客户权益
-    customers = get_customers()
-    revenues = get_customer_revenue()
+@statistics_api.get('/month/')
+async def get_month_revenue(query_date: str = Query(...)):  # 按月统计每人的客户指标情况(留存与利息是累加和，权益为最新一条记录)
+    start_timestamp, end_timestamp = get_month_range(query_date)
+    if start_timestamp == 0:
+        raise HTTPException(status_code=400, detail='参数`query_date`错误:can not format `%Y-%m-01`.')
+
+    customers, revenues = get_customers_and_revenues(start_timestamp, end_timestamp, 0)
     result = handle_customer_amount_revenue(pd.DataFrame(customers), pd.DataFrame(revenues))
-    return {'message': '统计成功!', 'statistics': result}
+    return {'message': '月统计成功!', 'statistics': result}
+
+
+@statistics_api.get('/year/')
+async def get_month_revenue(query_date: str = Query(...)):  # 按年统计每人的客户指标情况(留存与利息是累加和，权益为最新一条记录)
+    start_timestamp, end_timestamp = get_year_range(query_date)
+    if start_timestamp == 0:
+        raise HTTPException(status_code=400, detail='参数`query_date`错误:can not format `%Y-%m-01`.')
+
+    customers, revenues = get_customers_and_revenues(start_timestamp, end_timestamp, 0)
+    result = handle_customer_amount_revenue(pd.DataFrame(customers), pd.DataFrame(revenues))
+    return {'message': '年统计成功!', 'statistics': result}
 
 
 
